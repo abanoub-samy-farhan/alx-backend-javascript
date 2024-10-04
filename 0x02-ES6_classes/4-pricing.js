ed78cad71a41a68e27a9e5d796d67edabc3d8b6e
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-10-04 04:17:41 +0300

    init 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+export default class Pricing {
+    constructor(amount, currency) {
+        if (typeof amount === 'number') {
+            this._amount = amount;
+        } else throw TypeError("amount have to be a number");
+        if (typeof currency === 'string') {
+            this._currency = currency;
+        } else throw TypeError("currency have to be a string");
+    }
+
+    get amount() {
+        return this._amount;
+    }
+    set amount(newamount) {
+        if (typeof newamount === 'number') this._amount = newamount;
+        else throw TypeError("amount have to be a number");
+    }
+
+    get currency() {
+        return this._currency;
+    }
+    set currency(newcurrency) {
+        if (typeof newcurrency === 'string') this._currency = newcurrency;
+        else throw TypeError("currency have to be a string");
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency}`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        if (typeof amount === 'number' && typeof conversionRate === 'number') {
+            return amount * conversionRate;
+        } else {
+            throw TypeError("amount and conversionRate have to be a number");
+        }
+    }
+}
