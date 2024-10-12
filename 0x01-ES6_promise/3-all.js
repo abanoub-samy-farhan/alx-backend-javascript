97033b61f06b9bddccb6f890183f2722e2ddd191
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-10-12 22:59:55 +0300

    Promises done

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import {uploadPhoto, createUser} from "./utils"
+export default function handleProfileSignup(){
+    uploadPhoto().then((photo) => {
+        createUser().then((user) => {
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        })
+    })
+}
--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,12 +5,12 @@ export default class Car {
     this._color = color;
   }
 
-  static get [Symbol.species]() {
+  static get [Symbol.time]() {
     return this;
   }
 
   cloneCar() {
-    const NewCar = this.constructor[Symbol.species];
+    const NewCar = this.constructor[Symbol.specie];
     return new NewCar();
   }
 }
