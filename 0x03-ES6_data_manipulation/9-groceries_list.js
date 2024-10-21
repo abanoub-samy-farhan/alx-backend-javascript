118ac57a02b3c1556cea4fe161600dcbb40430fa
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-10-21 07:15:39 +0300

    task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const mymap = new Map();
+  mymap.set('Apple', 10);
+  mymap.set('Tomatoes', 10);
+  mymap.set('Pasta', 1);
+  mymap.set('Rice', 1);
+  mymap.set('Banana', 5);
+  return mymap;
+}
