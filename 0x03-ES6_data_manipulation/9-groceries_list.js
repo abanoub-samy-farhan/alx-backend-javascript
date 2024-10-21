f31efcb09d8dde9efe9e293500042148f1dd2962
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-10-21 07:17:17 +0300

    edit typos

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,6 +1,6 @@
 export default function groceriesList() {
   const mymap = new Map();
-  mymap.set('Apple', 10);
+  mymap.set('Apples', 10);
   mymap.set('Tomatoes', 10);
   mymap.set('Pasta', 1);
   mymap.set('Rice', 1);
