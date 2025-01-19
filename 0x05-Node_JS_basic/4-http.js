26a4c2ba263cc1b7c501389ef7721641eb211972
AbanoubAziz <abanoubsamy2341@gmail.com>
2025-01-19 18:46:50 +0200

    simple http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('node:http');
+
+const app = http.createServer();
+
+app.on('request', (req, res) => {
+  const responseMsg = 'Hello Holberton School!';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseMsg.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseMsg));
+});
+
+app.listen(12345, 'localhost', () => {
+  console.log('Server is listening on port 12345');
+});
